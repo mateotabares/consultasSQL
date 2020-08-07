f1ce66096eeaddb31291d00a13e5659466ddbad1
Mateo Tabares <53884114+mateotabares@users.noreply.github.com>
2020-08-07 17:12:15 -0500

    Actualizacion de prueba
    Se agregan metodos propios del conector mysql para simplificar el proceso

--- Prueba.py
+++ Prueba.py
@@ -2,9 +2,11 @@ import mysql.connector
 from tabulate import tabulate
 
 
-def mostrar_tabla(lista, cursor):
+def mostrar_tabla(cursor):
+    # Visualizar las tablas en un formato mas atractivo incluyendo el titulo de las mismas
     nombrescolumnas = [i[0] for i in cursor.description]
-    print(tabulate(lista, headers=nombrescolumnas, tablefmt='psql'))
+    res = cursor.fetchall()
+    print(tabulate(res, headers=nombrescolumnas, tablefmt='psql'))
 
 
 try:
@@ -14,13 +16,11 @@ try:
         password="MySQL",
         database="prueba"
     )
-    my_cursor = mydb.cursor()
-    conexion = 'exitosa'
+    my_cursor = mydb.cursor(buffered=True)
 except:
     print("La conexion con la base de datos no fue exitosa")
-    conexion = 'Fallida'
 
-while conexion == 'exitosa':
+while mydb.is_connected():
     try:
         eleccion = str(input(
             'Elige 1 si quieres conocer la poblacion de un pais \n Elige 2 si quieres conocer la poblacion de un '
@@ -28,7 +28,7 @@ while conexion == 'exitosa':
         if eleccion not in ('1', '2', '3'):
             raise Exception
     except:
-        print("Al parecer el valor que ingresaste no es valido. Intenta de nuevo")
+        print("Al parecer el valor que ingresaste no es valido. Intenta de nuevo \n \n")
         continue
 
     if eleccion == '1':
@@ -37,11 +37,11 @@ while conexion == 'exitosa':
         query = "SELECT DISTINCT tp.state,tp.city,tp.population FROM tabla_poblacion tp WHERE tp.country = '{}' ".format(
             pais)
         my_cursor.execute(query)
-        resultado = my_cursor.fetchall()
-        if resultado:
+
+        if my_cursor.rowcount:
             print(
                 "Has escogido {}. Este pais cuenta con una poblacion distribuida de la siguiente forma: ".format(pais))
-            mostrar_tabla(resultado, my_cursor)
+            mostrar_tabla(my_cursor)
         else:
             print("No se han encontrado resultados para el pais {}. Comprueba que esta bien escrito".format(pais))
         break
@@ -50,11 +50,10 @@ while conexion == 'exitosa':
         estado = str(input('Ingresa el nombre del estado que quieres consultar \n'))
         query = "SELECT DISTINCT tp.city,tp.population FROM tabla_poblacion tp WHERE tp.state = '{}' ".format(estado)
         my_cursor.execute(query)
-        resultado = my_cursor.fetchall()
-        if resultado:
+        if my_cursor.rowcount:
             print("Has escogido {}. Este estado cuenta con una poblacion distribuida de la siguiente forma: ".format(
                 estado))
-            mostrar_tabla(resultado, my_cursor)
+            mostrar_tabla(my_cursor)
         else:
             print("No se han encontrado resultados para el estado {}. Comprueba que esta bien escrito".format(estado))
         break
@@ -63,14 +62,13 @@ while conexion == 'exitosa':
         ciudad = str(input('Ingresa el nombre de la ciudad que quieres consultar \n'))
         query = "SELECT DISTINCT tp.city, tp.population FROM tabla_poblacion tp WHERE tp.city = '{}' ".format(ciudad)
         my_cursor.execute(query)
-        resultado = my_cursor.fetchall()
-        if resultado:
+        if my_cursor.rowcount:
             print("Has escogido {}. Esta ciudad cuenta con una poblacion distribuida de la siguiente forma: ".format(
                 ciudad))
-            mostrar_tabla(resultado, my_cursor)
+            mostrar_tabla(my_cursor)
         else:
             print("No se han encontrado resultados para la ciudad {}. Comprueba que esta bien escrito".format(ciudad))
         break
 
-if conexion == 'Exitosa':  # Si la conexion fue exitosa, se cierra la conexion antes de terminar.
+if mydb.is_connected():  # Si la conexion fue exitosa, se cierra la conexion antes de terminar.
     mydb.close()
