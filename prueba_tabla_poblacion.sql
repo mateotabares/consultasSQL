0a5a7a93f2df298776aaad85fcbc9e6c1a8e95ea
Mateo Tabares <53884114+mateotabares@users.noreply.github.com>
2020-08-06 20:32:04 -0500

    Add files via upload
    Importante que en el sistema de gestión de base de datos ya haya una base de datos llamada "prueba".

--- prueba_tabla_poblacion.sql
+++ prueba_tabla_poblacion.sql
@@ -1,3 +1,4 @@
+
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: prueba
@@ -51,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-06 20:12:34
+-- Dump completed on 2020-08-06 20:17:01
